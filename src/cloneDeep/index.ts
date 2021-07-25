0833c72261e92ed1ce61526b4472e321dda594a3
张聪 <zhangcong@192.168.0.101>
2021-07-25 22:59:01 +0800

    add deep-clone simple

--- src/cloneDeep/index.ts
+++ src/cloneDeep/index.ts
@@ -1,1 +1,22 @@
-export default {};
+/**
+ * 深克隆
+ * @author Conrad
+ * @param source 原数据
+ * @returns 克隆后的数据
+ */
+function cloneDeep(source: any) {
+  if (source === null) return null;
+  if (source === undefined) return undefined;
+
+  if (typeof source === "object") {
+    const target = {};
+    for (const key in target) {
+      target[key] = cloneDeep(source[key]);
+    }
+    return target;
+  } else {
+    return source;
+  }
+}
+
+export default cloneDeep
--- src/get-type/index.ts
+++ src/get-type/index.ts
@@ -1,4 +1,4 @@
-import getObjectType from './_utils/getObjectType';
+import getObjectType from './getObjectType';
 import { TypeEnum } from './types';
 
 // 截取"[object type]"的起始位置
@@ -9,7 +9,7 @@ const OBJECT_TYPE_SLICE_INDEX_END = -1;
 /**
  * @desc 获取对象的类型
  * @author Conrad
- * @param {obj} 需要检测类型的对象
+ * @param {*} obj 需要检测类型的对象
  * @return {TypeEnum} 对象的类型
  */
 const getType = (obj: any): TypeEnum => {
