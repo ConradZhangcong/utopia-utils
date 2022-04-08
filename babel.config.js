d357dd399899c1ce83a53fc775de6104c8fec15c
ConradZhangcong <heisenberg0519@outlook.com>
2022-04-08 11:40:49 +0800

    chore: optimize build

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
     '@babel/preset-typescript',
+    ['@babel/preset-env', { targets: { node: 'current' } }],
   ],
-  // ignore: ['node_modules', '__test__/'],
 };
